409110a8432c9f6c1fbf160e79bababdcf9076d5
Hans-Joachim Belz <hans-joachim.belz@anstrengungslos.de>
2025-02-23 21:15:15 +0100

    Component for Concept Node is almost done: (Almost) correct rendering of Key and Sub Concepts.

--- app/components/ConceptNode.tsx
+++ app/components/ConceptNode.tsx
@@ -6,17 +6,26 @@ export interface ConceptNodeProps {
   titleVariant?: string;
   subtitle: string;
   isKeyConcept?: boolean;
+  phenomenon?: Phenomenon;
   children: React.ReactNode;
 }
 
-const ConceptNode = ({ title, titleVariant, subtitle, isKeyConcept, children }: ConceptNodeProps): React.JSX.Element => {
+enum Phenomenon {
+  Observable = "observable",
+  Cognitive = "cognitive",
+  Trouble = "trouble"
+}
+
+const ConceptNode = ({ title, titleVariant, subtitle, isKeyConcept = false, phenomenon = Phenomenon.Observable, children }: ConceptNodeProps): React.JSX.Element => {
+  let stateSuffix = `${phenomenon}${isKeyConcept ? '--key-concept' : ''}`;
   return (
-    <div className={`concept-node${isKeyConcept ? ' concept-node--key-concept' : ''}`}>
+    <div className={`concept-node--${stateSuffix}`}>
       <div className="concept-node__header-box">
-        <h2 className="concept-node__title">{title} {titleVariant && <span className="concept-node__title-variant">({titleVariant})</span>}</h2>
-        <h3 className="concept-node__subtitle">{subtitle}</h3>
+        <h2><span className={`concept-node__title--${stateSuffix}`}>{title}</span>&nbsp; 
+        {titleVariant && <span className={`concept-node__title-variant--${stateSuffix}`}>({titleVariant})</span>}</h2>
+        <h3><span className={`concept-node__subtitle--${stateSuffix}`}>{subtitle}</span></h3>
       </div>
-      <div className="concept-node__text-box">
+      <div className={`concept-node__text-box--${stateSuffix}`}>
         <p>{children}</p>
       </div>
     </div>
