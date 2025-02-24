9cd5706f31f08fdd747be58516ead9421bf921ca
Hans-Joachim Belz <hans-joachim.belz@anstrengungslos.de>
2025-02-24 13:07:50 +0100

    Full support for key concepts including icons.

---
+++ app/components/concepts/ConceptNode.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import './ConceptNode.css';
+
+import ActionIcon from './icons/Action.svg';
+import AdjacencyPairIcon from './icons/Adjacency Pair.svg';
+import ConversationIcon from './icons/Conversation.svg';
+import EpistemicsIcon from './icons/Epistemics.svg';
+import InstitutionalIcon from './icons/Institutional.svg';
+import IntersubjectivityIcon from './icons/Intersubjectivity.svg';
+import ProjectIcon from './icons/Project.svg';
+import SequenceIcon from './icons/Sequence.svg';
+import TCUIcon from './icons/TCU.svg';
+import TurnIcon from './icons/Turn.svg';
+import UndefinedIcon from './icons/Undefined.svg';
+
+export const Icons: Record<string, string> = {
+  "Action": ActionIcon,
+  "AdjacencyPair": AdjacencyPairIcon,
+  "Conversation": ConversationIcon,
+  "Epistemics": EpistemicsIcon,
+  "Institutional": InstitutionalIcon,
+  "Intersubjectivity": IntersubjectivityIcon,
+  "Project": ProjectIcon,
+  "Sequence": SequenceIcon,
+  "TCU": TCUIcon,
+  "Turn": TurnIcon,
+  "Undefined": UndefinedIcon,
+};
+
+
+export enum Phenomenon {
+  Observable = "observable",
+  Cognitive = "cognitive",
+  Trouble = "trouble"
+}
+
+export interface ConceptNodeProps {
+  phenomenon?: Phenomenon;
+  title: string;
+  titleVariant?: string;
+  subtitle: string;
+  isKeyConcept?: boolean;
+  icon?: string;
+  children: React.ReactNode;
+}
+
+const ConceptNode = ({ phenomenon = Phenomenon.Observable, isKeyConcept = false, icon = "Undefined", title, titleVariant, subtitle, children }: ConceptNodeProps): React.JSX.Element => {
+  let stateSuffix = `${phenomenon}${isKeyConcept ? '--key-concept' : ''}`;
+  let iconResource = (Icons[icon] !== undefined ? Icons[icon] : Icons["Undefined"]);
+  return (
+    <div className={`concept-node--${stateSuffix}`}>
+      <div className="concept-node__header-box">
+        {isKeyConcept && <img className="concept-node__icon" src={iconResource} alt={phenomenon} />}
+        <div className="concept-node__title-rows">
+          <h2><span className={`concept-node__title--${stateSuffix}`}>{title}</span>&nbsp;
+            {titleVariant && <span className={`concept-node__title-variant--${stateSuffix}`}>({titleVariant})</span>}</h2>
+          <h3><span className={`concept-node__subtitle--${stateSuffix}`}>{subtitle}</span></h3>
+        </div>
+      </div>
+      <div className={`concept-node__text-box--${stateSuffix}`}>
+        <p>{children}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ConceptNode;
